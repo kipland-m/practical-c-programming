a1f4285e626fac6da6e2ad6f2ceafecaa9529faf
Kipland Melton <kiplandmelton@gmail.com>
2025-01-12 21:11:19 -0600

    5-10 example

---
+++ chapter5/examples/5-10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char line[100];
+float height;
+float width;
+float area;
+
+int main(){
+    printf("enter width height: ");
+    fgets(line, sizeof(line), stdin);
+
+    sscanf(line, "%f %f", &width, &height);
+
+    area = (width * height) / 2;
+    
+    printf("The area is %.2f\n", area);
+    return (0);
+}
