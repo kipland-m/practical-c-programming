67bcea4d0e54066d8d9aa0ab17dd33970811ac80
Kipland Melton <kiplandmelton@gmail.com>
2025-01-12 18:19:52 -0600

    const.c messing around declaring const

---
+++ chapter5/const/const.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+const float PI = 3.1415927;
+
+// you could use a constant for example,
+// allocating the size of and array
+const int TOTAL_MAX = 50;
+
+int main() {
+    return (0);
+}
+
