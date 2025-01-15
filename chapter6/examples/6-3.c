c169c509f77067abb6e42c8d4d4649fb436e2a4c
kipland-m <kiplandmelton@gmail.com>
2025-01-14 19:01:16 -0600

    started chapter7 and messing with makefiles

--- chapter6/examples/6-3.c
+++ chapter6/examples/6-3.c
@@ -9,25 +9,62 @@ int main(){
   sscanf(line, "%f", &grade);
 
   if (grade > 0){
-    if (grade < 60){
+    if (grade < 60)
+
       printf("F\n");
-    }}
+  }
+
+
   if (grade > 60){
-    if (grade < 70){
-      printf("D\n");
-    }}
+    if (grade < 70)
+
+    if (grade < 64)
+      printf("D-\n");
+    if (grade > 67)
+      printf("D+\n");
+
+    else{
+        printf("D\n");
+    }
+  }
+
   if (grade > 70){
-    if (grade < 80){
+    if (grade < 80)
+
+    if (grade < 74)
+      printf("C-\n");
+    if (grade > 77)
+      printf("C++\n");
+
+    else{
       printf("C\n");
-    }}
+    }
+  }
+
   if (grade > 80){
-    if (grade < 90){
+    if (grade < 90)
+
+    if (grade < 83)
+      printf("B-\n");
+    if (grade > 87)
+      printf("B+\n");
+
+    else{
       printf("B\n");
-    }}
-  if (grade > 90){
+    }
+  }
+
+  if (grade > 90) {
+    if (grade < 93)
+      printf("A-\n");
+    if (grade > 97)
+      printf("A+\n");
+
+    else{
       printf("A\n");
     }
+  }
 
   return (0);
-}
 
+}
---
+++ chapter7/calc/calc1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+char line[100];
+int result;
+char operator;
+int value;
+
+int main(){
+  result = 0;
+
+  while (1) {
+    printf("Result: %d\n", result);
+
+    printf("Enter operator and number: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%c %d", &operator, &value);
+
+    if (operator == 'q') {
+      break;
+    }
+    if (operator == '+') {
+      result += value;
+    } else {
+      printf("Unknown operator: %c\n", operator);
+    }
+  }
+
+  return (0);
+}
