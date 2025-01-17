d05df8fafebd9d6d1f046c3b966e7b63d0a0fa4e
Kipland Melton <kiplandmelton@gmail.com>
2025-01-16 23:24:00 -0600

    update, starting on date arithemtic

---
+++ chapter7/exercises/7-2/7-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+Write a program to perform date arithmetic such as how many days there are between 6/6/90 and 4/3/92. Include a specification and a code design.
+
+Date arithmetic has always baffled me.
+How am I supposed to translate this to number of days?
+
+Lets start small.
+
+How many days are between 6/6/90 and 7/4/90?
+
+To determine if a year is a leap year, you can divide it by 4. If it's evenly divisible by 4, it's a leap year. However, if the year is also divisible by 100, you need to check if it's also divisible by 400.
+*/
+
+// array of 12 integers that represent the amount of days in each month
+int year[12];
+
+int main(){
+  return (0);
+}
